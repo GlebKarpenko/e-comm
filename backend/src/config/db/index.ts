34aded816ee74c12a43d6cef5044fe1d248de049
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-19 20:39:55 +0200

    change mysql connection to pool

--- backend/src/config/db/index.ts
+++ backend/src/config/db/index.ts
@@ -1,22 +1,15 @@
-import mysql from 'mysql2/promise';
-import { Logger } from '@app/config/Logger';
+import mysql from 'mysql2';
+import config from '@app/config/config';
+import { Logger } from '../Logger';
 
-export const connectDB = async (): Promise<void> => {
+const pool = mysql.createPool(config.mysql).promise();
+
+export const connectDB = async () => {
     try {
-        const connection = await mysql.createConnection({
-            host: process.env.MYSQL_HOST,
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_DB
-        });
-
-        const [rows] = await connection.execute('SELECT DATABASE() AS db_name');
+        const [rows] = await pool.query("SELECT DATABASE() AS db_name");
         const dbName = (rows as mysql.RowDataPacket[])[0]?.db_name || 'Unknown';
-
         Logger.info(`Connected to database: ${dbName}`);
-
-        await connection.end();
     } catch (error) {
-        Logger.error("Database connection failed: ", error);
+        Logger.error(`Could not connect to database.`, 'DATABASE', error);
     }
 }
