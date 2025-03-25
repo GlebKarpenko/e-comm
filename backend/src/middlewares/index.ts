87936ef98ad88af5ff502cfe311481906cdc139f
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-25 17:03:17 +0200

    create authentication middlewares

---
+++ backend/src/middlewares/index.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import passport from '@app/config/auth/oauth';
+
+export const googleAuth = passport.authenticate('google', {
+    scope: ['profile', 'email']
+});
+
+export const googleAuthCallback = async (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('google', { failureRedirect: '/' }, (err, user) => {
+        if (err || !user) {
+            return res.redirect("/login?error=Authentication failed");
+        }
+
+        req.logIn(user, (loginErr) => {
+            if (loginErr) {
+                return next(loginErr);
+            }
+            return res.redirect('/');
+        });
+    })(req, res, next);
+}
+
+export const isAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.isAuthenticated()) {
+        return res.redirect('/profile');
+    }
+    next();
+}
