c563429024ddee3180cac9a9e003f843deccba98
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-25 17:04:27 +0200

    create authentication routes

---
+++ backend/src/router/auth.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { googleAuth, googleAuthCallback } from "@app/middlewares";
+import { getProfile, logout } from "@app/controllers/auth";
+
+export default (router: Router): void => {
+    router.get("/auth/google", googleAuth);
+    router.get("/auth/google/callback", googleAuthCallback);
+    router.get("/auth/logout", logout);
+    router.get("/auth/profile", getProfile);
+};
