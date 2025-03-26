af5b1db5e57fd4b60384a31021f5873e95ef9b6c
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-26 17:41:37 +0200

    update list of tables and their primary keys

--- backend/src/interfaces/tables.ts
+++ backend/src/interfaces/tables.ts
@@ -1,3 +1,13 @@
 export enum Tables {
     USER = 'user',
+    PRODUCT = 'product',
+    CATEGORY = 'product_category',
+    DISCOUNT = 'discount',
+}
+
+export const TableIds = {
+    user: 'id_user',
+    product: 'id_product',
+    product_category: 'id_product_category',
+    discount: 'id_discount',
 }
