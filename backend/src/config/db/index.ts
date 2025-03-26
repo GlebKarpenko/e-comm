037e17b4ad2b449ba8655952d24c1325084659b3
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-26 17:40:24 +0200

    make basic db interface methods

--- backend/src/config/db/index.ts
+++ backend/src/config/db/index.ts
@@ -1,6 +1,7 @@
 import mysql from 'mysql2';
 import config from '@app/config/config';
-import { Logger } from '../Logger';
+import { Logger } from '../../utils/Logger';
+import { TableIds, Tables } from '@app/interfaces/tables';
 
 export const pool = mysql.createPool(config.mysql).promise();
 
@@ -22,4 +23,69 @@ export const executeQuery = async <T=any>(query: string, params: any[] = []): Pr
         Logger.error(`Could not execute query.`, 'DATABASE', error);
         throw new Error(`Database operation failed. ${error}`);
     }
+}
+
+export const getAll = async <T=any>(table: Tables): Promise<T[]> => {
+    return executeQuery<T[]>(
+        `SELECT * FROM ${table}`
+    );
+}
+
+export const getById = async <T=any>(id: number, table: Tables): Promise<T> => {
+    const result = await executeQuery<T[]>(
+        `SELECT * FROM ${table} WHERE ${TableIds[table]} = ?`, 
+        [id]
+    );
+
+    return result[0];
+}
+
+export const create = async <T=any>(data: Record<string, any>, table: Tables): Promise<T> => {
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+
+    const fieldNames = fields.map(field => `\`${field}\``).join(", ");
+    const placeholders = fields.map(() => '?').join(", ");
+
+    const result = await executeQuery(
+        `INSERT INTO ${table} (${fieldNames}) VALUES (${placeholders})`,
+        values
+    );
+
+    return getById(result.insertId, table);
+}
+
+export const update = async <T=any>(id: number, data: Record<string, any>, table: Tables, ): Promise<T> => {
+    const fields = Object.keys(data);
+    const values = Object.values(data);
+
+    if (fields.length === 0) {
+        throw new Error('No fields to insert');
+    }
+
+    const setClause = fields.map(field => `\`${field}\` = ?`).join(", ");
+    
+    await executeQuery(
+        `UPDATE ${table}
+        SET ${setClause}
+        WHERE ${TableIds[table]} = ?`,
+        [...values, id]
+    );
+
+    return getById(id, table);
+}
+
+export const remove = async <T=any>(id: number, table: Tables): Promise<T> => {
+    const deletedItem = await getById(id, table);
+
+    if (!deletedItem) {
+        throw new Error("Item not found.");
+    }
+
+    await executeQuery(
+        `DELETE FROM ${table} WHERE ${TableIds[table]} = ?`,
+        [id]
+    );
+
+    return deletedItem;
 }
