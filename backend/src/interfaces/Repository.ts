ac8796dfc6da31b1ed240c88186fb2cc00c526c7
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-26 17:41:12 +0200

    create DTO interfaces for repositories

---
+++ backend/src/interfaces/Repository.ts
@@ -0,0 +1,84 @@
+export interface User {
+    id_user: number,
+    username: string,
+    email: string,
+    provider: string,
+    provider_id: string,
+    role: 'customer' | 'admin',
+    created_at: string,
+    modified_at: string,
+}
+
+export interface ModifyUserDTO {
+    username: string,
+    email: string,
+    provider: string,
+    provider_id: string,
+    role: 'customer' | 'admin',
+}
+
+export interface ProductCategory {
+    id_product_category: number,
+    name: string,
+    desc: string,
+    created_at: string,
+    modified_at: string,
+    deleted_at: string,
+}
+
+export interface ModifyCategoryDTO {
+    name: string,
+    desc: string,
+}
+
+export interface Discount {
+    id_discount: number,
+    name: string,
+    desc: string,
+    percentage: number,
+    is_active: boolean,
+    created_at: string,
+    modified_at: string,
+    deleted_at: string,
+}
+
+export interface ModifyDiscountDTO {
+    name: string,
+    desc: string,
+    percentage: number,
+    is_active: boolean,
+}
+
+export interface Product {
+    id_product: number,
+    name: string,
+    slug: string,
+    desc: string,
+    SKU: string,
+    image: string,
+    image_mime: string,
+    image_size: number,
+    status: string,
+    price: number,
+    category: ProductCategory,
+    inventory_quantity: number,
+    discount: Discount,
+    created_at: string,
+    modified_at: string,
+    deleted_at: string,
+}
+
+export interface ModifyProductDTO {
+    name: string,
+    slug: string,
+    desc: string,
+    SKU: string,
+    image: string,
+    image_mime: string,
+    image_size: string,
+    status: string,
+    price: number,
+    id_category: number,
+    inventory_quantity: number,
+    id_discount: number,
+}
