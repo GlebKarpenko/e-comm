c22977002462732b9611c157882c3145407d5559
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-03-27 11:34:13 +0200

    use proper naming for modify DTO

--- backend/src/repositories/SessionRepository.ts
+++ backend/src/repositories/SessionRepository.ts
@@ -1,9 +1,9 @@
 import { IRepository } from "./IRepository";
 import { Tables } from "@app/interfaces/tables";
 import * as db from "@app/config/db"
-import { ShoppingSession, ShoppingSessionDTO } from "@app/interfaces/Repository";
+import { ShoppingSession, ModifySessionDTO } from "@app/interfaces/Repository";
 
-export class SessionRepository implements IRepository<ShoppingSession, ShoppingSessionDTO> {
+export class SessionRepository implements IRepository<ShoppingSession, ModifySessionDTO> {
     private readonly tableName = Tables.SESSION;
 
     async getAll(): Promise<ShoppingSession[]> {
@@ -14,11 +14,11 @@ export class SessionRepository implements IRepository<ShoppingSession, ShoppingS
         return db.getById(sessionId, this.tableName);
     }
 
-    async create(sessionData: ShoppingSessionDTO): Promise<ShoppingSession> {
+    async create(sessionData: ModifySessionDTO): Promise<ShoppingSession> {
         return db.create(sessionData, this.tableName);
     }
 
-    async update(sessionId: number, updatedData: Partial<ShoppingSessionDTO>): Promise<ShoppingSession> {
+    async update(sessionId: number, updatedData: Partial<ModifySessionDTO>): Promise<ShoppingSession> {
         return db.update(sessionId, updatedData, this.tableName);
     }
 
