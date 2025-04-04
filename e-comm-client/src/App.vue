a3f71f873c195f54e27ca98d52da12af1d8eba86
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-04-04 19:52:44 +0300

    create project structure

--- e-comm-client/src/App.vue
+++ e-comm-client/src/App.vue
@@ -1,7 +1,9 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Main</router-link> |
   </nav>
   <router-view/>
 </template>
