05e7e352f919f3f13aacd110b665212104dbe239
Карпенко Глеб <glebkarpenko1@gmail.com>
2025-04-10 20:39:39 +0300

    add filters side-panel to products-page layout

--- e-comm-client/src/views/products-page/ProductsView.vue
+++ e-comm-client/src/views/products-page/ProductsView.vue
@@ -4,6 +4,7 @@ import { ref, onMounted } from 'vue';
 import { ProductCpu } from './types/product.types';
 import { fetchAllProducts } from './api/products';
 import ProductsLayout from './components/ProductsLayout.vue';
+import FilterPanel from './components/FilterPanel.vue';
 
 const { t } = useI18n({ useScope: 'global' });
 const I18Namespace = 'products-page';
@@ -28,22 +29,39 @@ onMounted(async () => {
 <template>
  <div class="products-view">
   <h1 class="page-heading">{{ t(`${I18Namespace}.heading`) }} <i class="fas fa-microchip"></i></h1>
-  <div class="products-wrapper">
-    <ProductsLayout :products="products"/>
-  </div>
+  <main class="products-content">
+    <aside class="products-filter">
+      <FilterPanel />
+    </aside>
+
+    <section class="products-grid-wrapper">
+      <ProductsLayout :products="products"/>
+    </section>
+  </main>
  </div>
 </template>
 
 <style scoped lang="scss">
-.products-wrapper {
+.products-content {
+  display: flex;
+  gap: 1.5rem; // add space between filter and grid
   max-width: 1440px;
   margin: 0 auto;
   padding-left: 1rem;
   padding-right: 1rem;
 }
 
+.products-filter {
+  flex: 0 0 240px; // fixed width sidebar
+}
+
+.products-grid-wrapper {
+  flex: 1; // take all remaining space for grid
+  min-width: 0; // important for overflow control
+}
+
 @media (min-width: 1024px) {
-  .page-wrapper {
+  .products-content {
     padding-left: 2rem;
     padding-right: 2rem;
   }
